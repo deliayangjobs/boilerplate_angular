f68691c0cfc8dec4803927d747e22e8c2c838f02
Delia Yang <deliayangjobs@gmail.com>
2018-03-25 21:13:06 -0700

    created dangular project

---
+++ dangular/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  courses;
+
+  loadCourses() {
+      this.courses = [
+          {id:1, name:'course1'},
+          {id:2, name:'course2'},
+          {id:3, name:'course3'}
+      ];
+  }
+
+  trackCourse(index, course) {
+      return course ? course.id: null;
+  }
+}
